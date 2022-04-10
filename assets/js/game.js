10859de4c29559b43e569303493def96e90765d5
Timothy Staub <timstaub41@gmail.com>
2022-04-10 11:07:18 -0400

    Module 3.1.5 learning to create & execute functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,20 @@
-window.alert("This is an alert!  JavaScript is running!");
+
+
+// this creates a funtion named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+//fight();
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+// "Tony the Robot is ready for battle"
+console.log(playerName + " is ready for battle!");
+//"Your robot, Tony the Robot, has won!"
+console.log("Your robot, " + playerName + ", has won!");
